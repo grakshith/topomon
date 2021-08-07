6790a47e6955aaa6c075a8fd06cff179c6213ad5
Rakshith Gopala Krishna <rakshith.gopalakrishna@algorand.com>
2021-08-07 00:11:53 -0700

    Client done to an extent

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import  Graph from "graphology";
 import randomLayout from "graphology-layout/random";
 import noverlap, { NoverlapNodeReducer } from "graphology-layout-noverlap";
+import { Attributes, EdgeKey, NodeKey } from "graphology-types";
 import { animateNodes } from "sigma/utils/animate";
+import FA2Layout from "graphology-layout-forceatlas2/worker";
+import NoverlapLayout from 'graphology-layout-forceatlas2/worker';
 import { Sigma } from "sigma";
 import { globalize } from "./utils";
 
@@ -11,10 +14,31 @@ const container = document.getElementById("sigma-container");
 // Create the graph structure
 const graph = new Graph();
 
-// randomize the layout
-// randomLayout.assign(graph);
+let highlightedNodes = new Set();
+let highlightedEdges = new Set();
+
+const nodeReducer = (node: NodeKey, data: Attributes) => {
+  if(highlightedNodes.has(node)){
+    return { ...data, color: "#f00", zIndex: 1};
+  }
+
+  return data;
+}
+
+const edgeReducer = (edge: EdgeKey, data: Attributes) => {
+  if(highlightedEdges.has(edge)){
+    return { ...data, color: "#f00", zIndex: 1};
+  }
+
+  return data;
+}
+
 // SIgma settings
-const settings = {};
+const settings = {
+  nodeReducer: nodeReducer,
+  edgeReducer: edgeReducer,
+  zIndex: true,
+};
 
 // Sigma init & render
 const renderer = new Sigma(graph, container, settings);
@@ -32,6 +56,9 @@ const NOVERLAP_SETTINGS = {
   speed: 3,
 };
 
+const layout = new FA2Layout(graph, { settings: { slowDown: 1000000 } });
+layout.start();
+
 const inputReducer: NoverlapNodeReducer = (key, attr) => {
   return { ...attr, ...renderer.graphToViewport(attr) };
 };
@@ -42,6 +69,8 @@ const outputReducer: NoverlapNodeReducer = (key, attr) => {
 
 // setup websocket connection
 const ws = new WebSocket('ws://localhost:8080');
+
+// Event listeners
 
 ws.addEventListener('open', function(event){
     console.log("ws: Connection opened - ", event);
@@ -81,3 +110,33 @@ ws.addEventListener('message', function(event){
     // const layout = noverlap(graph, { inputReducer, outputReducer, maxIterations: 500, settings: NOVERLAP_SETTINGS });
     // animateNodes(graph, layout, { duration: 100, easing: "linear" }, null);
 });
+
+renderer.on("clickNode", ({node, captor, event}) => {
+  console.log("Clicking: ", node, captor, event);
+  var neighbors = graph.neighbors(node);
+  neighbors.forEach(neighbor => {
+    highlightedNodes.add(neighbor);    
+  });
+  highlightedNodes.add(node);
+  var edges = graph.edges(node);
+  edges.forEach(edge => {
+    highlightedEdges.add(edge);
+  });
+
+  renderer.refresh();
+
+  // pan camera to node
+  renderer.getCamera().animate(renderer.getNodeDisplayData(node) as { x: number; y: number }, {
+    easing: "linear",
+    duration: 500,
+  });
+
+});
+
+renderer.on("clickStage", ({node, captor, event})=>{
+  console.log("Clicking stage: ", node, captor, event);
+  highlightedNodes.clear();
+  highlightedEdges.clear();
+  renderer.getCamera().animatedReset({duration: 500});
+  renderer.refresh();
+})
