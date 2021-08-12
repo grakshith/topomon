7823d17bba8a90e30e3a223f55afd2601fee549e
Rakshith Gopala Krishna <rakshith.gopalakrishna@algorand.com>
2021-08-12 16:36:55 -0700

    Add fields for es query service

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/algorand/websocket v1.4.2
+	github.com/elastic/go-elasticsearch/v6 v6.8.10
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/sirupsen/logrus v1.8.1
--- src/server/config.go
+++ src/server/config.go
@@ -5,14 +5,24 @@ import (
 )
 
 type Config struct {
-	Host string `json:"host"`
-	Port string `json:"port"`
+	Host              string `json:"host"`
+	Port              string `json:"port"`
+	TelemetryEndpoint string `json:"telemetryEndpoint"`
+	MetricsEndpoint   string `json:"metricsEndpoint"`
+	Channel           string `json:"channel"`
+	Network           string `json:"network"`
 }
 
 var DefaultLocalConfig = Config{
-	Host: "",
-	Port: "8080",
+	Host:              "",
+	Port:              "8080",
+	TelemetryEndpoint: "http://telemetry.rakshith-s1.algodev.network:9105",
+	MetricsEndpoint:   "http://telemetry.rakshith-s1.algodev.network:9090",
+	Channel:           "rakshith-s1",
+	Network:           "rakshith-s1",
 }
+
+var CurrentConfig = DefaultLocalConfig
 
 func (config Config) BuildBindAddr() string {
 	var bindAddr strings.Builder
@@ -20,4 +30,14 @@ func (config Config) BuildBindAddr() string {
 	bindAddr.WriteString(":")
 	bindAddr.WriteString(config.Port)
 	return bindAddr.String()
+}
+
+func (config Config) BuildESIndexName() string {
+	var indexName strings.Builder
+	indexName.WriteString(config.Channel)
+	indexName.WriteString("-")
+	indexName.WriteString(config.Network)
+	indexName.WriteString("-")
+	indexName.WriteString("v1")
+	return indexName.String()
 }
